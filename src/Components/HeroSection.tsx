7974a0f1489e1aaaebca3cc0ced923530d1d2b65
Rameez Murtaza <rameezmurtaza5@gmail.com>
2024-12-12 16:28:10 +0500

    sixth commit

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -7,27 +7,25 @@ function Hero() {
       <Image
         src="/images/hero.png"
         alt="Hero Background"
-        fill
-        style={{objectFit: "cover"}} 
+        layout="fill"
+        objectFit="cover"
         priority
         className="z-[-1]"
       />
 
       {/* Text content on the right side */}
-      <div className="absolute top-12 right-0 sm:top-16 md:top-28 md:right-0 bg-[#FFF3E3] m-4 p-6 sm:p-8 md:p-10 w-[90%] sm:w-[420px] md:w-[620px] h-auto md:h-[380px] text-black">
-        <p className="uppercase text-xs sm:text-sm font-semibold">
-          New Arrival
-        </p>
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight mt-2 text-[#B88E2F]">
+      <div className="absolute top-28 right-0 p-8 bg-[#FFF3E3] w-[620px] h-[380px] text-black m-8">
+        <p className="uppercase text-sm font-semibold">New Arrival</p>
+        <h1 className="text-5xl font-bold leading-tight mt-2 text-[#B88E2F]">
           Discover Our
           <br />
           New Collection
         </h1>
-        <p className="text-xs sm:text-sm md:text-base mt-4 mb-4">
+        <p className="text-sm mt-4 mb-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
           tellus, luctus nec ullamcorper mattis.
         </p>
-        <button className="bg-[#B88E2F] text-white mt-6 sm:mt-9 py-2 sm:py-3 px-8 sm:px-14 hover:bg-[#a0522d] transition">
+        <button className="bg-[#B88E2F] text-white mt-9 py-3 px-14 hover:bg-[#a0522d] transition">
           Buy Now
         </button>
       </div>
